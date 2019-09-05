27c2a9159e72ec5a16c0125bae2fba0a6adcc0b8
nqrcqn <47343000+nqrcqn@users.noreply.github.com>
2019-09-05 18:07:18 +0300

    Update PercentEnergyShieldFilter.cs

--- Procurement/ViewModel/Filters/ForumExport/PercentEnergyShieldFilter.cs
+++ Procurement/ViewModel/Filters/ForumExport/PercentEnergyShieldFilter.cs
@@ -5,10 +5,10 @@ using System.Text;
 
 namespace Procurement.ViewModel.Filters.ForumExport
 {
-    public class PercentEnergyShieldFilter : StatFilter
+    public class PercentEnergyShieldFilter : OrStatFilter
     {
         public PercentEnergyShieldFilter()
-            : base("% Increased Energy Shield", "Items with % increased energy shield", "% increased maximum energy shield")
+            : base("% Increased Energy Shield", "Items with % increased energy shield", "% increased maximum Energy Shield", "increased Global Defences")
         { }
 
         public override FilterGroup Group
