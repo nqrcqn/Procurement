22e56255f79d6140479e7d7d2e7b4256faa59360
nqrcqn <47343000+nqrcqn@users.noreply.github.com>
2019-09-05 16:14:05 +0300

    Create CritMultiplierFilter.cs

---
+++ Procurement/ViewModel/Filters/ForumExport/CritMultiplierFilter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class CritMultiplierFilter : ExplicitModBase
+    {
+        public CritMultiplierFilter()
+            : base("Critical Strike Multiplier")
+        { }
+
+        public override bool CanFormCategory
+        {
+            get { return false; }
+        }
+
+        public override string Keyword
+        {
+            get { return "Critical Strike Multiplier"; }
+        }
+
+        public override string Help
+        {
+            get { return "Items with increased Critical Strike Multiplier"; }
+        }
+
+        public override FilterGroup Group
+        {
+            get { return FilterGroup.Crit; }
+        }
+    }
+}
