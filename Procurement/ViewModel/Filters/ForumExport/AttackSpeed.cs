5ab080a60c256a0598d5137ffe07dfe53c841327
nqrcqn <47343000+nqrcqn@users.noreply.github.com>
2019-09-05 15:18:40 +0300

    Update AttackSpeed.cs

--- Procurement/ViewModel/Filters/ForumExport/AttackSpeed.cs
+++ Procurement/ViewModel/Filters/ForumExport/AttackSpeed.cs
@@ -8,7 +8,7 @@
         }
 
         public AttackSpeed()
-            : base("Increased Attack Speed", "Increased Attack Speed", "Increased Attack Speed")
+            : base("Increased Attack Speed", "Increased Attack Speed", "increased Attack Speed", "increased Attack and Cast Speed", "increased Attack, Cast and Movement Speed")
         { }
     }
 }
