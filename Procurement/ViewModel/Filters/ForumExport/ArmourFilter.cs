567604bceddb210aaca7137bfdd07df3f8a14d8a
nqrcqn <47343000+nqrcqn@users.noreply.github.com>
2019-09-05 18:31:18 +0300

    Create ArmourFilter.cs

---
+++ Procurement/ViewModel/Filters/ForumExport/ArmourFilter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class ArmourFilter : OrStatFilter
+    {
+        public ArmourFilter()
+            : base("Armour", "Items with Armour", "to Armour", "increased Armour", "increased Global Armour", "increased Global Defences")
+        { }
+
+        public override FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+    }
+}
