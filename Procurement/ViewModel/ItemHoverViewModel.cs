a731df2c2a1837f6601e15b5ef209c936a1ad2a3
nqrcqn <47343000+nqrcqn@users.noreply.github.com>
2019-09-19 13:05:37 +0300

    Update ItemHoverViewModel.cs

--- Procurement/ViewModel/ItemHoverViewModel.cs
+++ Procurement/ViewModel/ItemHoverViewModel.cs
@@ -164,7 +164,7 @@ namespace Procurement.ViewModel
 
             bool HasMods = HasFracturedMods || HasExplicitMods || HasCraftedMods || HasVeiledMods || IsMirrored || IsUnidentified || IsCorrupted;
 
-            if ((Properties != null || UtilityMods != null) && (HasRequirements || HasEnchantMods || HasImplicitMods || HasMods || HasMicrotransactions || FlavourText != null || DescriptionText != null || IsIncubatorProgressVisible))
+            if ((Properties != null || UtilityMods?.Count > 0) && (HasRequirements || HasEnchantMods || HasImplicitMods || HasMods || HasMicrotransactions || FlavourText != null || DescriptionText != null || IsIncubatorProgressVisible))
             {
                 this.SeparateProperties = true;
             }
