e87508bbde18799e6127ff35916e451eb082420d
nqrcqn <47343000+nqrcqn@users.noreply.github.com>
2019-10-05 14:37:52 +0300

    Update Hybrid.cs

--- POEApi.Model/Hybrid.cs
+++ POEApi.Model/Hybrid.cs
@@ -5,16 +5,16 @@ namespace POEApi.Model
     public class Hybrid
     {
         public bool IsVaalGem { get; set; }
-        public List<Property> VaalProperties { get; set; }
-        public List<string> VaalExplicitMods { get; set; }
-        public string VaalSecDescrText { get; set; }
+        public List<Property> Properties { get; set; }
+        public List<string> ExplicitMods { get; set; }
+        public string SecDescrText { get; set; }
 
         internal Hybrid(JSONProxy.Hybrid proxy)
         {
             IsVaalGem = proxy.IsVaalGem;
-            VaalProperties = proxy.Properties;
-            VaalExplicitMods = proxy.ExplicitMods;
-            VaalSecDescrText = proxy.SecDescrText;
+            Properties = proxy.Properties;
+            ExplicitMods = proxy.ExplicitMods;
+            SecDescrText = proxy.SecDescrText;
         }
     }
 }
