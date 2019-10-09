edef6d17c87911f45d37f527871f42f48af119e9
nqrcqn <47343000+nqrcqn@users.noreply.github.com>
2019-10-09 15:55:03 +0300

    Update ForumThreadException.cs

--- POEApi.Infrastructure/ForumThreadException.cs
+++ POEApi.Infrastructure/ForumThreadException.cs
@@ -2,24 +2,12 @@
 
 namespace POEApi.Infrastructure
 {
-    private string errormessage;
-
     public class ForumThreadException : Exception
     {
         public ForumThreadException()
             : base() { }
 
         public ForumThreadException(string message)
-            : base(message)
-        {
-            errormessage = message;
-        }
-    }
-    public override string Message
-    {
-        get
-        {
-            return errormessage;
-        }  
+            : base(message) { }
     }
 }
