fb2402bbfab3f28764676487e25db29ab1717a22
nqrcqn <47343000+nqrcqn@users.noreply.github.com>
2019-10-16 10:17:13 +0300

    Update StashViewModel.cs

--- Procurement/ViewModel/TabViewModel/StashViewModel.cs
+++ Procurement/ViewModel/TabViewModel/StashViewModel.cs
@@ -180,7 +180,7 @@ namespace Procurement.ViewModel
             }
 			else
 			{
-			    stashView.tabControl.BringIntoView(stashView.tabControl.SelectedItem);
+			    stashView.tabControl.SelectedItem.BringIntoView();
 			}
         }
 
