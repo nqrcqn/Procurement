2e7fd5e88c8c991b0b844940b6ce81831b299fad
nqrcqn <47343000+nqrcqn@users.noreply.github.com>
2019-10-17 16:56:32 +0300

    Update ItemDisplayViewModel.cs

--- Procurement/ViewModel/ItemDisplayViewModel.cs
+++ Procurement/ViewModel/ItemDisplayViewModel.cs
@@ -267,12 +267,12 @@ namespace Procurement.ViewModel
             if (item.SocketedItems == null || item.SocketedItems.Count == 0)
                 return false;
 
-            return item.SocketedItems.Exists(i => i.Socket == socketIndex && (socket.Attribute == i.Color || socket.Attribute == "G" || i.Color == "G" || socket.Attribute == "A" && i.Color == null));
+            return item.SocketedItems.Exists(i => i.Socket == socketIndex && (socket.Attribute == "A" && i.Color == null || socket.Attribute == i.Color || socket.Attribute == "G" || i.Color == "G"));
         }
 
         private SocketableItem getSocketItemAt(Socket socket, int socketIndex, Gear item)
         {
-            return item.SocketedItems.First(i => i.Socket == socketIndex && (socket.Attribute == i.Color || socket.Attribute == "G" || i.Color == "G" || socket.Attribute == "A" && i.Color == null));
+            return item.SocketedItems.First(i => i.Socket == socketIndex && (socket.Attribute == "A" && i.Color == null || socket.Attribute == i.Color || socket.Attribute == "G" || i.Color == "G"));
         }
 
         private Image GetSocket(Socket socket, string suffix)
